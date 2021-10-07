d078c006c979358c6015030065807c23d84f35d5
Bas Heemskerk <bash32957@gmail.com>
2021-10-07 18:25:00 +0200

    Added some extra stuff
    Added the office move script to the other games. did some work on FNaF1 office

---
+++ Assets/Scripts/Shared/officeScript_1_2_3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class officeScript_1_2_3 : MonoBehaviour {
+
+	[Header("Office variables")]
+	public Transform officeTransform;
+	public float moveSpeed;
+	public int[] camLimit;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Joystick1Button11) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			officeTransform.localPosition = new Vector3(officeTransform.localPosition.x + moveSpeed, officeTransform.localPosition.y, officeTransform.localPosition.z);
+		}
+		else if (Input.GetKey(KeyCode.Joystick1Button9) || Input.GetKey(KeyCode.RightArrow))
+		{
+			officeTransform.localPosition = new Vector3(officeTransform.localPosition.x - moveSpeed, officeTransform.localPosition.y, officeTransform.localPosition.z);
+		}
+
+		if (officeTransform.localPosition.x < camLimit[0]) officeTransform.localPosition = new Vector3(camLimit[0], officeTransform.localPosition.y, officeTransform.localPosition.z);
+		if (officeTransform.localPosition.x > camLimit[1]) officeTransform.localPosition = new Vector3(camLimit[1], officeTransform.localPosition.y, officeTransform.localPosition.z);
+	}
+}
