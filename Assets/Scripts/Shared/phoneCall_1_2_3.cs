2221873b573464fec5508e8fb52cf05a61c04cf9
Bas Heemskerk <bash32957@gmail.com>
2021-10-10 09:23:36 +0200

    working on phone system

---
+++ Assets/Scripts/Shared/phoneCall_1_2_3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class phoneCall_1_2_3 : MonoBehaviour {
+
+	public GameObject _endCall;
+	public AudioSource _callAudioSource;
+	public AudioClip[] _phoneCalls;
+	public bool inCall = true;
+
+
+	[Header("Shared scripts")]
+	public whichNight _nightScript;
+
+	// Use this for initialization
+	void Start () {
+		startCall();
+	}
+
+	public void startCall()
+    {
+		_endCall.SetActive(true);
+		_callAudioSource.clip = _phoneCalls[_nightScript._whichNight - 1];
+		_callAudioSource.Play();
+    }
+
+	public void endCall()
+    {
+
+    }
+	
+	void Update () {
+		
+	}
+}
